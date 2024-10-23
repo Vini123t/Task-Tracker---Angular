d11fb65c76062b1d6bb1e8bf058a5c50279a5ebe
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-10-23 20:02:11 -0300

    a

--- TaskeTracker/src/app/header/header.component.ts
+++ TaskeTracker/src/app/header/header.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   imageUrl: string = 'assets/list.png';
+
 }
---
+++ TaskeTracker/src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomUser = () =>
+  DUMMY_USERS[Math.floor(
+    Math.random() * DUMMY_USERS.length
+  )];
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectUse = randomUser();
+
+}
