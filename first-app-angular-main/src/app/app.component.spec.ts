273a8736a743b246660fd7bb68b2fa54d80bb0b9
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-10-29 20:41:33 -0300

    teste

--- first-app-angular-main/src/app/app.component.spec.ts
+++ first-app-angular-main/src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'TaskeTracker' title`, () => {
+  it(`should have the 'first-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TaskeTracker');
+    expect(app.title).toEqual('first-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TaskeTracker');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first-app');
   });
 });
---
+++ first-app-angular-main/src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+  @Input({required: true}) id!: string;
+  @Input({required: true}) name!: string;
+  @Input({required: true}) avatar!: string;
+
+  @Output() select = new EventEmitter<string>();
+
+  onSelectedUser(){
+    this.select.emit(this.name);
+  }
+}
